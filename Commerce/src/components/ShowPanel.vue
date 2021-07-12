6faef5bcaba622b7885cb6a3c702bcdaac906c09
cjkchn <85102216+cjkchn@users.noreply.github.com>
2021-07-12 11:14:21 +0800

    蔡锦康前端修改与后台连接

--- Commerce/src/components/ShowPanel.vue
+++ Commerce/src/components/ShowPanel.vue
@@ -7,11 +7,13 @@
 				选择网点：<el-input v-model="input" placeholder="选择网点" clearable class="el-input-one-s"></el-input>
 			</div>
 			<div class="el-select-table-two-s">
-				<el-button @click="addCourseDetail()" style="background:#337ab7;border-color: #337ab7;color: #fff;"><i class="el-icon-search"></i> 搜索</el-button>
-				<el-button @click="toggleSelection()" style="background:#337ab7;border-color: #337ab7;color: #fff;"><i class="el-icon-circle-plus-outline"></i>添加</el-button>
-				<el-button :disabled="this.multipleSelection.length === 0"
+				<el-button @click="" style="background:#337ab7;border-color: #337ab7;color: #fff;"><i
+						class="el-icon-search"></i> 搜索</el-button>
+				<el-button @click="" style="background:#337ab7;border-color: #337ab7;color: #fff;"><i
+						class="el-icon-circle-plus-outline"></i>添加</el-button>
+				<!-- <el-button :disabled="this.multipleSelection.length === 0"
 					@click="deleteCourseDetail(multipleSelection)" type="danger" style="background:#337ab7;border-color: #337ab7;color: #fff;">- 批量删除
-				</el-button>
+				</el-button> -->
 			</div>
 		</el-col>
 	</el-row>
@@ -19,17 +21,15 @@
 		<el-col :span="23" class="el-table-show-one-s">
 			<div class="grid-content2 bg-purple-dark1">
 				<div>
-					<el-table :data="CourseDetailData" @selection-change="handleSelectionChange" stripe
-						class="el-table-one-s" ref="multipleTable" :header-cell-style="{background:'rgb(238, 241, 246) none repeat scroll 0% 0%',color:'#606266'}">
+					<el-table :data="CustomerData" @selection-change="handleSelectionChange" stripe
+						class="el-table-one-s" ref="multipleTable"
+						:header-cell-style="{background:'rgb(238, 241, 246) none repeat scroll 0% 0%',color:'#606266'}">
 						<el-table-column type="selection" min-width="50">
 						</el-table-column>
 						<el-table-column type="expand" max-width="15" label="详细" :show-overflow-tooltip="true">
 							<template #default="props">
 								<el-form label-position="left" inline class="demo-table-expand">
 									<div style="display: inline-block;" class="el-table-xiang-show-one-s">
-										<el-form-item label="考勤(应道/实到):">
-											<span>31/28</span>
-										</el-form-item>
 										<el-form-item label="课程费用:">
 											<span>{{ props.row.courseVo.courseMoney }}元</span>
 										</el-form-item>
@@ -50,7 +50,8 @@
 							<template #default="scope">
 								<el-button @click="showEdit(scope.row)" type="text" size="small">编辑</el-button>
 								<div class="ivu-divider"></div>
-								<el-button @click="delSelectAllCourseDetail(scope.row)" type="text" size="small">删除</el-button>
+								<el-button @click="delSelectAllCourseDetail(scope.row)" type="text" size="small">删除
+								</el-button>
 							</template>
 						</el-table-column>
 					</el-table>
@@ -66,47 +67,6 @@
 			</div>
 		</el-col>
 	</el-row>
-	<!-- 弹窗 -->
-	<el-dialog title="增加课类详细信息" v-model="dialogVisible" width="30%" :before-close="handleClose">
-		<el-form :model="CourseDetail" :rules="rules" ref="CourseDetail" label-width="100px" class="demo-ruleForm">
-			<el-form-item label="课程详细名:" prop="coursedetailsName">
-				<el-input v-model="CourseDetail.coursedetailsName" placeholder="请输入课程类别名称"></el-input>
-			</el-form-item>
-			<el-form-item label="课程名称:" style="padding-right: 85px;">
-				<el-select v-model="CourseDetail.courseId" placeholder="请选择">
-					<el-option v-for="item in CourseData" :key="item.courseId" :label="item.courseName"
-						:value="item.courseId">
-					</el-option>
-				</el-select>
-			</el-form-item>
-			<el-form-item label="课程序列号:">
-				<el-input v-model="CourseDetail.coursesequence" placeholder="请输入课时量"></el-input>
-			</el-form-item>
-			<el-form-item class="el-form-butt-show-one-s">
-				<el-button type="primary" @click="addCourseDetailData('CourseDetail')">立即创建</el-button>
-			</el-form-item>
-		</el-form>
-	</el-dialog>
-	<el-dialog title="修改课程详细信息" v-model="dialogVisible2" width="50%" :before-close="handleClose">
-		<el-form :model="CourseDetail" :rules="rules" ref="CourseDetail" label-width="100px" class="demo-ruleForm">
-			<el-form-item label="课程详细名:" prop="coursedetailsName">
-				<el-input v-model="CourseDetail.coursedetailsName" placeholder="请输入课程类别名称"></el-input>
-			</el-form-item>
-			<el-form-item label="课程名称:" style="padding-right: 85px;">
-				<el-select v-model="CourseDetail.courseId" placeholder="请选择">
-					<el-option v-for="item in CourseData" :key="item.courseId" :label="item.courseName"
-						:value="item.courseId">
-					</el-option>
-				</el-select>
-			</el-form-item>
-			<el-form-item label="课程序列号:">
-				<el-input v-model="CourseDetail.coursesequence" placeholder="请输入课时量"></el-input>
-			</el-form-item>
-			<el-form-item class="el-form-butt-show-one-s">
-				<el-button type="primary" @click="updateCourseDetail('CourseDetail')">立即修改</el-button>
-			</el-form-item>
-		</el-form>
-	</el-dialog>
 </template>
 
 <script>
@@ -131,49 +91,13 @@
 					]
 				},
 				multipleSelection: [],
-				dialogVisible: false,
-				dialogVisible2: false,
-				CourseDetail: {
-					coursedetailsId: "",
-					coursedetailsName: "",
-					coursesequence: "",
-					addname: "",
-					addtime: "",
-					updatename: "",
-					updatetime: "",
-					deletename: "",
-					deletetime: "",
-					timeliness: "",
-					courseId: ""
-				},
-				Course: {
-					courseId: "",
-					courseName: "",
-					courseState: "",
-					classhours: "",
-					courseMoney: "",
-					commission: "",
-					addname: "",
-					addtime: "",
-					updatename: "",
-					updatetime: "",
-					stopname: "",
-					stoptime: "",
-					beginname: "",
-					begintime: "",
-					deletename: "",
-					deletetime: "",
-					timeliness: "",
-					classtypeId: ""
-				},
 				pageInfo: {
 					currentPage: 1,
 					pagesize: 5,
 					total: 0
 				},
-				CourseData: [],
-				classtypeData: [],
-				CourseDetailData: [],
+				CustomerData: [],
+				Customer: [],
 				input: ''
 			}
 		},
@@ -185,97 +109,21 @@
 					})
 					.catch(_ => {});
 			},
-			addCourseDetailData(CourseDetail) {
-				this.Course.courseId =this.CourseDetail.courseId
-				/* console.log(this.Course.courseId) */
-				this.CourseDetail.addname = this.$store.state.loginname
-				this.CourseDetail.timeliness = 0
-				const _this = this
-				this.axios.put("http://localhost:8080/Training/updateByCourseHouse", this.Course)
-					.then(function(response) {
-						_this.FindCourseDetailData();
-					}).catch(function(error) {
-						console.log(error)
-					})
-				this.axios.post("http://localhost:8080/Training/addCourseDetailsService", this.CourseDetail)
-					.then(function(response) {
-						_this.FindCourseDetailData();
-						_this.dialogVisible = false
-					}).catch(function(error) {
-						console.log(error)
-					})
-			},
-			delSelectAllCourseDetail(row) {
-				const _this = this
-				this.CourseDetail.coursedetailsId = row.coursedetailsId
-				this.Course.courseId =row.courseId
-				this.CourseDetail.deletename = this.$store.state.loginname
-				var flag = true
-				this.$confirm('此操作将永久删除该数据, 是否继续?', '提示', {
-					confirmButtonText: '确定',
-					cancelButtonText: '取消',
-					type: 'warning'
-				}).then(() => {
-					this.axios.put("http://localhost:8080/Training/updateByCourseHouseTwo", this.Course)
-						.then(function(response) {
-							_this.FindCourseDetailData();
-						}).catch(function(error) {
-							console.log(error)
-						})
-					this.axios.put("http://localhost:8080/Training/deleteByCourseDetails", this.CourseDetail)
-						.then(function(response) {
-							_this.FindCourseDetailData();
-						}).catch(function(error) {
-							console.log(error)
-						})
-				}).catch(() => {
-					this.$message({
-						type: 'error',
-						message: '取消删除!'
-					});
-				});
-			},
 			handleSelectionChange(multipleSelection) {
 				this.multipleSelection = multipleSelection;
 			},
 			toggleSelection() {
 				this.$refs.multipleTable.clearSelection();
 			},
-			showEdit(row){
-				this.CourseDetail.courseId = row.courseId
-				this.CourseDetail.coursedetailsId = row.coursedetailsId
-				this.CourseDetail.coursedetailsName = row.coursedetailsName
-				this.CourseDetail.coursesequence = row.coursesequence
-				this.CourseDetail.updatename = this.$store.state.loginname
-				this.CourseDetail.addname = null
-				this.dialogVisible2 = true
-			},
-			updateCourseDetail(CourseDetail) {
-				this.$refs[CourseDetail].validate((valid) => {
-					if (valid) {
-						const _this = this
-						this.axios.put("http://localhost:8080/Training/updateByCourseDetailsKey", this.CourseDetail)
-							.then(function(response) {
-								_this.dialogVisible2 = false
-								_this.FindCourseDetailData();
-							}).catch(function(error) {
-								console.log(error)
-							})
-					} else {
-						console.log("修改失败！");
-						return false;
-					}
-				})
-			},
 			handleCurrentChange(page) {
 				var _this = this
 				this.pageInfo.currentPage = page;
-				this.axios.get("http://localhost:8080/Training/selectAllCourseDetails", {
+				this.axios.get("http://localhost:8089/Logistics/selectAllCustomer", {
 						params: this.pageInfo
 					})
 					.then(function(response) {
 						console.log(response)
-						_this.CourseDetailData = response.data.list
+						_this.CustomerData = response.data.list
 						_this.pageInfo.total = response.data.total
 					}).catch(function(error) {
 						console.log(error)
@@ -284,12 +132,12 @@
 			handleSizeChange(size) {
 				var _this = this
 				this.pageInfo.pagesize = size;
-				this.axios.get("http://localhost:8080/Training/selectAllCourseDetails", {
+				this.axios.get("http://localhost:8089/Logistics/selectAllCustomer", {
 						params: this.pageInfo
 					})
 					.then(function(response) {
 						console.log(response)
-						_this.CourseDetailData = response.data.list
+						_this.CustomerData = response.data.list
 						_this.pageInfo.total = response.data.total
 					}).catch(function(error) {
 						console.log(error)
@@ -302,51 +150,37 @@
 			FindCourseDetailData() {
 				var _this = this
 				console.log(this.pageInfo.currentPage, this.pageInfo.pagesize)
-				this.axios.get("http://localhost:8080/Training/selectAllCourseDetails", {
+				this.axios.get("http://localhost:8089/Logistics/selectAllCustomer", {
 						params: this.pageInfo
 					})
 					.then(function(response) {
 						console.log(response)
-						_this.CourseDetailData = response.data.list
+						_this.CustomerData = response.data.list
 						_this.pageInfo.total = response.data.total
 					}).catch(function(error) {
 						console.log(error)
 					})
 			},
-			deleteCourseDetail() {
-				var ids = this.multipleSelection.map(item => item.coursedetailsId).join()
-				console.log(ids)
-				this.$confirm('此操作将永久删除选中信息, 是否继续?', '提示', {
-					confirmButtonText: '确定',
-					cancelButtonText: '取消',
-					type: 'warning',
-					center: true
-				}).then(() => {
-					const _this = this
-					this.CourseDetail.deletename = this.$store.state.loginname
-					this.axios.put("http://localhost:8080/Training/deleteByCourseDetailsKey/" + ids+"/"+this.$store.state.loginname)
-						.then(function(response) {
-							_this.FindCourseDetailData();
-						}).catch(function(error) {
-							console.log(error)
-						})
-				}).catch(() => {
-					this.$message({
-						type: 'info',
-						message: '已取消删除'
-					});
-				});
-			},
 		},
 		created() {
 			var _this = this
-			this.axios.get("http://localhost:8088/Training/selectAllCourseDetails", {
+			this.axios.get("http://localhost:8089/Logistics/selectAllCustomer", {
 					params: this.pageInfo
 				})
 				.then(function(response) {
 					console.log(response)
-					_this.CourseDetailData = response.data.list
+					_this.CustomerData = response.data.list
 					_this.pageInfo.total = response.data.total
+				}).catch(function(error) {
+					console.log(error)
+				})
+			var _this = this
+			this.axios.get("http://localhost:8089/Logistics/selectAllCustomerList", {
+					params: this.Customer
+				})
+				.then(function(response) {
+					console.log(response)
+					_this.Customer = response.data.list
 				}).catch(function(error) {
 					console.log(error)
 				})
@@ -361,7 +195,7 @@
 		margin-bottom: 10px;
 		margin-top: 10px;
 		margin-left: 23px;
-		box-shadow: 0 0 0 0 #eaeaea,0 2px 2px 1px #eaeaea;
+		box-shadow: 0 0 0 0 #eaeaea, 0 2px 2px 1px #eaeaea;
 	}
 
 	.el-col {
